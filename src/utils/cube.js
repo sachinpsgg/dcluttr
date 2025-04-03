efd8b9515bef0164cf63ab04b0af2e69e7c69c6c
sachinpsgg <sachin@psglobalgroup.com>
2025-04-03 22:15:13 +0530

    env var added

--- src/utils/cube.js
+++ src/utils/cube.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
-
-const API_URL = 'https://amaranth-muskox.aws-us-east-1.cubecloudapp.dev/dev-mode/feat/frontend-hiring-task/cubejs-api/v1/load';
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJicmFuZElkIjoiNDkiLCJleHAiOjE3NDM0OTYyMTIsImlzcyI6ImN1YmVjbG91ZCJ9.luqfkt0CQW_B01j5oAvl_8hicbFzPmyLXfvEZYJbej4';
-
+const API_URL = import.meta.env.VITE_CUBE_API_URL;
+const TOKEN = import.meta.env.VITE_CUBE_API_TOKEN;
 export const fetchCubeData = async (query) => {
     try {
         const response = await axios.post(
